bb8ba10064ac6287c3884ccf7e5d9a8f32dd6698
Jacob Jakiemiec <jjakiemiec@gmail.com>
2025-01-31 20:33:00 -0900

    Jacob stops being dumb

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ class Program
         Console.WriteLine("try 2 for merging");
         Console.WriteLine("test account");
 
+        int num = 7;
+        int num2, num3 = 2, num4 = 3;
+        double db = 7.1;
+        char ch = (char)7;
+        string coolCatchphrase = "wowowowow";
+        bool isCool = true;
+        num2 = 2;
+
+        Console.WriteLine(num + db);
+        Console.Write(ch);
+
+        if (isCool)
+        {
+            Console.WriteLine(coolCatchphrase);
+        }
+        
         /*
          * this is a multiline comment
          * 
