2dbd2e8940fba7fa635c30c74ffa0428c6fe23d4
Jacob Jakiemiec <jjakiemiec@gmail.com>
2025-01-31 20:10:47 -0900

    add comment

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Jacob tries his hardest
         Console.WriteLine("IT sure do");
         Console.WriteLine(12 + 17);
         Console.WriteLine("try 2 for merging");
         Console.WriteLine("test account");
+
+        /*
+         * this is a multiline comment
+         * 
+         * 
+         * wowowowow
+         */
     }
 }
